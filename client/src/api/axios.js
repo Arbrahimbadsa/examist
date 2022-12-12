cd84c8c2c459dda69914d1d2caff251dbe3c6d52
Arb <arbrahimbadsa@gmail.com>
2022-12-12 22:43:12 +0600

    header added

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,4 +3,7 @@ import { HOST } from "../utils/hostname";
 
 export default axios.create({
   baseURL: HOST,
+  headers: {
+    "Access-Control-Allow-Origin": "https://flameyourskill.vercel.app",
+  },
 });
