4c4728b9748c1802df5e636e677d20e6e5fae01a
Arb <arbrahimbadsa@gmail.com>
2022-12-13 10:50:41 +0600

    vercel configuration

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origin: "https://flameyourskill.vercel.app",
   },
 });
 
